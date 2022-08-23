5f8d4121c12201ab1172e20041347520a0f7f70e
kamil.hapak <kamil.hapak@britenet.com.pl>
2022-08-23 12:22:27 +0200

    add POST order test

--- src/test/java/tacos/OrderControllerTest.java
+++ src/test/java/tacos/OrderControllerTest.java
@@ -1,25 +1,46 @@
 package tacos;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import tacos.web.OrderController;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @WebMvcTest(OrderController.class)
-public class OrderControllerTest {
+class OrderControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-
-    public void testOrderPage()  {
-
-
+    void testOrderPage() throws Exception {
+        // given
+        Order order = new Order();
+        // when
+        mockMvc.perform(
+                post("/orders")
+                        .contentType(APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(order))
+                        .accept(APPLICATION_JSON))
+               // then
+                .andExpect(status().isOk());
     }
 }
 
